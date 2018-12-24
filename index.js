6890a17b5ae3f916a45a6415cf402fd8ec1b027f
Robert Snyder <r.snyder49@gmail.com>
2018-12-24 23:38:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -36,3 +36,6 @@ function greet(customerOne, customerTwo) {
 greet.call(sally, 'Terry', 'George');
 // Hi Terry and George, my name is Sally!
 
+greet.call(sally);
+// Hi undefined and undefined, my name is Sally!
+
