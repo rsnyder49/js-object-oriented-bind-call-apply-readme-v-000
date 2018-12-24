68a579ae5e31b5634b1ff0d8b5efc7552f3e1964
Robert Snyder <r.snyder49@gmail.com>
2018-12-24 23:35:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,3 +10,15 @@ let person = {
 };
  
 person.greet(); // my name is Bob, hi!
+
+function greet() {
+    console.log(`my name is ${this.name}, hi!`);
+}
+ 
+let sally = { name: 'Sally' };
+ 
+greet.call(sally);
+// my name is Sally, hi!
+ 
+greet.apply(sally);
+// my name is Sally, hi!
