fcc370530ac24400046e9dfc98fb4979f9b89fc5
Robert Snyder <r.snyder49@gmail.com>
2018-12-24 23:44:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -84,3 +84,30 @@ let freeMusic = new Event('Free Music Show', ['music', 'free', 'outside']);
  
 billy.matchInterests(freeMusic);
 // Uncaught TypeError: Cannot read property 'interests' of undefined
+
+class Event {
+    constructor(title, keywords) {
+        this.title = title;
+        this.keywords = keywords;
+    }
+}
+ 
+class User {
+    constructor(name, interests) {
+        this.name = name;
+        this.interests = interests;
+    }
+ 
+    matchInterests(event) {
+        return event.keywords.some(
+            function(word) {
+                return this.interests.includes(word);
+            }.bind(this) // added to the and of the callback function
+        );
+    }
+}
+ 
+let billy = new User('billy', ['music', 'art', 'movies']);
+let freeMusic = new Event('Free Music Show', ['music', 'free', 'outside']);
+ 
+billy.matchInterests(freeMusic);
