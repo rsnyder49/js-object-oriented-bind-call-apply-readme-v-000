2015cfa5cf4b660008af6973be647200f81967f0
Robert Snyder <r.snyder49@gmail.com>
2018-12-24 23:38:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -27,5 +27,12 @@ function greet(customerOne, customerTwo) {
     console.log(`Hi ${customerOne} and ${customerTwo}, my name is ${this.name}!`);
 }
 
-
+let sally = { name: 'Sally' };
+ 
+function greet(customerOne, customerTwo) {
+    console.log(`Hi ${customerOne} and ${customerTwo}, my name is ${this.name}!`);
+}
+ 
+greet.call(sally, 'Terry', 'George');
+// Hi Terry and George, my name is Sally!
 
