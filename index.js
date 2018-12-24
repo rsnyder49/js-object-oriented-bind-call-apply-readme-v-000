9ae03ed17c58b22135279dcdcd2d242eb5bcc2b5
Robert Snyder <r.snyder49@gmail.com>
2018-12-24 23:42:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -58,3 +58,29 @@ greet('Bob');
 
 greet.bind(sally)('Bob');
 // Hi Bob, my name is Sally!
+
+class Event {
+    constructor(title, keywords) {
+        this.title = title;
+        this.keywords = keywords;
+    }
+}
+ 
+class User {
+    constructor(name, interests) {
+        this.name = name;
+        this.interests = interests;
+    }
+ 
+    matchInterests(event) {
+        return event.keywords.some(function(word) {
+            return this.interests.includes(word);
+        });
+    }
+}
+ 
+let billy = new User('billy', ['music', 'art', 'movies']);
+let freeMusic = new Event('Free Music Show', ['music', 'free', 'outside']);
+ 
+billy.matchInterests(freeMusic);
+// Uncaught TypeError: Cannot read property 'interests' of undefined
